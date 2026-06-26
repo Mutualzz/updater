425ad395240ae663d97297877e84c39f65a13f19
Azrael <azrael@mutualzz.com>
2026-06-26 02:33:58 -0700

    Added icon for the updater

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ fn main() {
     #[cfg(target_os = "windows")]
     {
         let mut res = winres::WindowsResource::new();
+        res.set_icon("./resources/icon.ico");
         res.set_manifest(r#"
 <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
   <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
